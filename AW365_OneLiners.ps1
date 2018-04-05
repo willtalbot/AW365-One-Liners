10e0554d046446327ba423b282c4f07327c59a82
Talbot Will (1Plex) <E154319@dir.ucb-group.com>
2018-04-05 12:31:48 +0200

    filter

--- AW365_OneLiners.ps1
+++ AW365_OneLiners.ps1
@@ -2,4 +2,4 @@
 Get-CsTrustedApplication | select identity, Port | ft -AutoSize
 #****************************************************************************************
 #   Get List of Trusterdapplications filtered by SIP address
-Get-CsTrustedApplicationEndpoint -Filter {sipaddress -like "*UCC_UCBC*"} | select Sipaddress, DisplayName, LineURI, VoicePolicy | FT -AutoSize
+Get-CsTrustedApplicationEndpoint -Filter {sipaddress -like "*UCC_*"} | select Sipaddress, DisplayName, LineURI, VoicePolicy | FT -AutoSize
